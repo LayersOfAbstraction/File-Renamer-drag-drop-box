104c4de530e72d021c81aad7a49b4bcffb6a22bb
Jordan Nash <jnash51@live.com>
2016-06-18 21:38:31 +1000

    C# "Can't find specified the file"
    This just a simple project I'm making to rename files dynamically when
    they're put in the drag drop box. Meaning we don't have to use an
    absolute path. we do it all in the Drag Drop box that appears on the
    form.
    
    It can't find the file. Does anyone know what the problem is please. I'm
    new to this.

---
+++ DragDrop/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private string getFileName(string path)
+        {            
+            return Path.GetFileName(path);
+        }
+
+        private string getDirectoryName(string path)
+        {
+            return Path.GetDirectoryName(path);
+        }
+
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            //TAKE dropped items and store in array.
+            string[] dropppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            //LOOP through all droppped items and display them
+            foreach (string file in dropppedFiles)
+            {
+                string filename = getFileName(file);
+                MessageBox.Show("You dropped " + filename);
+                listBox1.Items.Add(filename);
+                FileInfo fi = new FileInfo(filename);
+                {
+                    if (!File.Exists("NewName"))
+                    {
+                        getDirectoryName(file);
+                        fi.MoveTo("NewName");
+                        //File.Move(filename, getDirectoryName(file));
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        
+    }
+}
