b14da89aef35afbc2e4bbb700b6416dd4836f2da
Jordan Nash <jnash51@live.com>
2018-11-21 15:24:56 +1000

    Application now in Beta Release

--- DragDrop/Form1.Designer.cs
+++ DragDrop/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace DragDropForm
 {
     partial class Form1
     {
@@ -28,36 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstDragDrop = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstDragDrop
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 167);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(284, 95);
-            this.listBox1.TabIndex = 0;
+            this.lstDragDrop.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstDragDrop.FormattingEnabled = true;
+            this.lstDragDrop.Location = new System.Drawing.Point(0, 179);
+            this.lstDragDrop.Name = "lstDragDrop";
+            this.lstDragDrop.Size = new System.Drawing.Size(430, 95);
+            this.lstDragDrop.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bradley Hand ITC", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filename Time and Date Converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bradley Hand ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(342, 150);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(23, 150);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(299, 20);
+            this.txtFolderPath.TabIndex = 4;
             // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.listBox1);
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(430, 274);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstDragDrop);
             this.Name = "Form1";
             this.Text = "Form1";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstDragDrop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtFolderPath;
     }
 }
 
--- DragDrop/Form1.cs
+++ DragDrop/Form1.cs
@@ -2,25 +2,41 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace DragDropForm
 {
     public partial class Form1 : Form
     {
+        static string[] _dropppedFiles;
+        string _strSourcePath;
+        string _strTargetPath;
+        string _strfileName = string.Empty;
+        string _strDestFile = string.Empty;
+
         public Form1()
         {
             InitializeComponent();
+            readLabelMsg();
         }
 
-        
-        private string getFileName(string path)
-        {            
+
+        private void readLabelMsg()
+        {
+            label2.Text = "1. Please click and drag files into big drag drop box below." +
+            "\n2. Type your folder path into the text box next to convert button." +
+            "\n3. Press button to convert files in the big drag drop box.";
+        }
+
+        private string getFileName( string path)
+        {
             return Path.GetFileName(path);
         }
 
@@ -29,29 +45,16 @@ namespace WindowsFormsApplication1
             return Path.GetDirectoryName(path);
         }
 
-
         private void Form1_DragDrop(object sender, DragEventArgs e)
         {
             //TAKE dropped items and store in array.
-            string[] dropppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+            _dropppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
 
             //LOOP through all droppped items and display them
-            foreach (string file in dropppedFiles)
+            foreach (string file in _dropppedFiles)
             {
-                string filename = getFileName(file);
-                MessageBox.Show("You dropped " + filename);
-                listBox1.Items.Add(filename);
-                FileInfo fi = new FileInfo(filename);
-                {
-                    if (!File.Exists("NewName"))
-                    {
-                        getDirectoryName(file);
-                        fi.MoveTo("NewName");
-                        //File.Move(filename, getDirectoryName(file));
-                    }
-                }
+                lstDragDrop.Items.Add(file);
             }
-
         }
 
         private void Form1_DragEnter(object sender, DragEventArgs e)
@@ -62,6 +65,35 @@ namespace WindowsFormsApplication1
             }
         }
 
-        
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            _strTargetPath = txtFolderPath.Text;
+            RenameFile(_strTargetPath);
+        }
+
+        public void RenameFile(string targetDirectory)
+        {
+            //Get file regardless of specific extension.
+            foreach(string file in _dropppedFiles)
+            {
+                if (!lstDragDrop.Items.Contains(_dropppedFiles.Length))
+                {
+                    //GET file extension.
+                    var extension = Path.GetExtension(file);
+                    _strfileName = File.GetCreationTime(file).ToString("yyyy'-'MM'-'dd hh'\u0589'mm'\u0589'ss tt", CultureInfo.InvariantCulture);
+                    _strfileName += extension;
+                    _strSourcePath = getDirectoryName(file);                  
+                    //GET Creation Date and time from file.                                        
+                    _strDestFile = Path.Combine(targetDirectory, _strfileName);
+                    //Copy file with extension to output directory.
+                    File.Copy(file, _strDestFile, true);
+                }
+
+                else
+                {
+                    Debug.WriteLine("Source path does not exist!");
+                }
+            }
+        }
     }
 }
